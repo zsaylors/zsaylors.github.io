5d16b0e46704310b8ae5a49eb7f671d54cd442cf
Zak Saylors <zsaylors11@gmail.com>
2019-09-04 20:15:21 -0600

    Initial commit

---
+++ resources/js/script.js
@@ -0,0 +1,12 @@
+new fullpage('#fullpage', {
+            anchors: ['zs'],
+            menu: '#menu',
+            navigationTooltips: ['main', 'projects', 'skills', 'about', 'dummy'],
+            css3: true,
+            scrollingSpeed: 600,
+            slidesNavigation: true,
+            responsiveHeight: 300,
+            dragAndMove: true,
+            dragAndMoveKey: 'YWx2YXJvdHJpZ28uY29tX0EyMlpISmhaMEZ1WkUxdmRtVT0wWUc=',
+            controlArrows: false 
+});
